07f679a408753d820a1658425544a7c6a56f2a92
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2023-10-26 22:20:32 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,25 +37,6 @@ function App() {
   const [company_id,setCompanyId] = useState("");
   const [job_id,setJobId] = useState("");
   const [file,setFile] = useState(null);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('user_id',user_id);
-    formData.append('company_id',company_id);
-    formData.append('job_id',job_id);
-    formData.append('file',file);
-    console.log(formData)
-    axios.post('http://localhost:5000/api/postApply',formData,{
-      headers: {
-        'Content-Type':'multipart/form-data'
-      }
-    })
-    .then(res=>{
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
-    })
-  }
   return (
     <div className="App">
       <ul>
@@ -64,14 +45,6 @@ function App() {
       {showModal && (
         <Modals currentCv={currentCv} showModal={showModal} handleCloseModal={handleCloseModal}/>
       )}
-      {/* <PdfViewer /> */}
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={user_id} onChange={(e)=>{setUserId(e.target.value)}} />
-        <input type='text' value={company_id} onChange={(e)=>{setCompanyId(e.target.value)}} />
-        <input type='text' value={job_id} onChange={(e)=>{setJobId(e.target.value)}} />
-        <input type='file' onChange={(e)=>{setFile(e.target.files[0])}} />
-        <input type='submit' />
-      </form>
     </div>
   );
 }
